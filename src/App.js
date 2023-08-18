56d7fc172455d6b007681bfbf66a21d89757172f
Aleksander_Panfilov <panfilov.a.e@mail.ru>
2023-08-18 15:47:02 +0300

    ClassCounter

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
+import Counter from "./components/counter";
 
 function App() {
-  const [likes, setLikes] = useState(1);
-  const [value, setValue] = useState("Пусто");
-
-  function increment() {
-      setLikes(likes + 1);
-  }
-
-  function decrement() {
-      setLikes(likes - 1);
-  }
-
   return (
-   
-    <div className="App">
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input 
-      type="text" 
-      value={value} 
-      onChange={(e) => setValue(e.target.value)}
-      />
-      <button onClick={increment}>increment</button>
-      <button onClick={decrement}>decrement</button>
-    </div>
-  );
+    <Counter />
+  )
 }
 
 export default App;
---
+++ src/components/counter.jsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+
+function Counter(){
+  const [likes, setLikes] = useState(1);
+  const [value, setValue] = useState("Пусто");
+
+  function increment() {
+      setLikes(likes + 1);
+  }
+
+  function decrement() {
+      setLikes(likes - 1);
+  }
+
+  return (
+   
+    <div className="App">
+      <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input 
+      type="text" 
+      value={value} 
+      onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+    </div>
+  );
+}
+export default Counter;
